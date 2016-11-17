bc62e91cae058b96efe1606ac53e09beb4dfa7f4
manju <manjub4u58@gmail.com>
2016-11-18 00:01:47 +0530

    Ng resource is added

--- mytest/src/main/webapp/resources/js/controller.js
+++ mytest/src/main/webapp/resources/js/controller.js
@@ -1,7 +1,54 @@
- app.controller('myController', function ($scope,userService) {
-           
-
+ app.controller('myController', function ($scope,userService,Category) {
+	 $scope.categorys=[];     
+	 $scope.categorys =Category.get();
+	 
 $scope.add=function(){
  $scope.message = userService.mul(2); 
-              }			
+              }	
+
+$scope.getCategory=function()
+{
+	
+   $scope.catObj = Category.get({id:24},
+   function() {
+   console.log('User name is ');
+   },
+   function(){
+   alert("Hello! I am an alert box!!");
+   console.log('User could not came');
+   }
+   ); 
+   
+   $scope.something={
+			  "data": [
+			    {
+			      "pkId": 24,
+			      "createdBy": 1,
+			      "createdOn": 1478952315000,
+			      "lastModifiedOn": 1479231441000,
+			      "name": "Food is updated",
+			      "description": "Here you will get all king of food",
+			      "imgUrl": "hhttp//:Url",
+			      "active": true,
+			      "lastModifiedBy": 1
+			    }
+			  ],
+			  "self": "Get:mytest/admin/categorys/24"
+    }
+	
+}
+
+$scope.getAllCategory=function()
+{
+ }
+$scope.saveCategory=function()
+{
+ }
+$scope.updateCategory=function()
+{
+ }
+
+$scope.deleteCategory=function()
+{
+ }
         });
--- mytest/src/main/webapp/resources/js/services.js
+++ mytest/src/main/webapp/resources/js/services.js
@@ -1,3 +1,20 @@
+
+app.factory('Category', ['$resource', function ($resource) {
+    //$resource() function returns an object of resource class
+    return $resource(
+            'http://localhost:8080/mytest/admin/categorys/:id', 
+            {id: '@id'},
+            {
+                update: {
+                      method: 'PUT' // To send the HTTP Put request when calling this custom update method.
+                }
+                 
+            }
+    );
+}]);
+
+
+
 app.service('userService', function(){
 	 this.mul = function(a) {
 	 return a + a
